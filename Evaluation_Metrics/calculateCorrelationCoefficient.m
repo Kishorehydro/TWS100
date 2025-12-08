f9705d202fcc1bdbda3465c60cde20669a7b9beb
Kishorehydro <nabakishor_g@ce.iitr.ac.in>
2025-12-08 09:03:53 +0530

    Delete Evaluation_Metrics directory

--- Evaluation_Metrics/calculateCorrelationCoefficient.m
+++
@@ -1,9 +0,0 @@
-function cc = calculateCorrelationCoefficient(app,y1, y2)
-            % Calculate correlation coefficient
-            if length(y1) < 2
-                cc = NaN;
-                return;
-            end
-            R = corrcoef(y1, y2);
-            cc = R(1,2);
-        end
