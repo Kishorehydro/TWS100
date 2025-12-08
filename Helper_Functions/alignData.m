9e1fa129dcbff11bccd3b6b9a97e676d02f96b4a
Kishorehydro <nabakishor_g@ce.iitr.ac.in>
2025-12-08 09:06:12 +0530

    Delete Helper_Functions directory

--- Helper_Functions/alignData.m
+++
@@ -1,6 +0,0 @@
-function [y1_aligned, y2_aligned] = alignData(app,y1, y2)
-            % Align two datasets by removing time points where either has NaN
-            validMask = ~isnan(y1) & ~isnan(y2);
-            y1_aligned = y1(validMask);
-            y2_aligned = y2(validMask);
-        end
--- Helper_Functions/isvalidField.m
+++
@@ -1,4 +0,0 @@
-% Helper function to safely test if a field exists and is valid
-function tf = isvalidField(app, propName)
-    tf = isprop(app, propName) && ~isempty(app.(propName)) && isvalid(app.(propName));
-end
